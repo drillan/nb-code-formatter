db13592d08294bd8ec8581c1365c06e56d9e891d
driller <eleshis@gmail.com>
2022-06-11 15:24:44 +0900

    add type hints

--- nb_code_formatter/__init__.py
+++ nb_code_formatter/__init__.py
@@ -1,32 +1,35 @@
+from __future__ import annotations
 import re
+import pathlib
 
 import black
 import click
 import isort
 import nbformat
+from nbformat.notebooknode import NotebookNode
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 NBFORMAT_VERSION = 4
 
 
-def black_formatter(cell_text):
-    cell_text = re.sub("^%", "#%#", cell_text, flags=re.M)
-    reformated_text = black.format_str(cell_text, mode=black.FileMode())
+def black_formatter(cell_text: str) -> str:
+    drop_magic_text: str = re.sub("^%", "#%#", cell_text, flags=re.M)
+    reformated_text: str = black.format_str(drop_magic_text, mode=black.FileMode())
     return re.sub("^#%#", "%", reformated_text, flags=re.M)
 
 
-def format_code(cell_text):
+def format_code(cell_text: str) -> str:
     return isort.code(black_formatter(cell_text))
 
 
-def format_nb(nb):
+def format_nb(nb: NotebookNode) -> NotebookNode:
     for cell in nb["cells"]:
         if cell["cell_type"] == "code":
             cell["source"] = format_code(cell["source"])
     return nb
 
 
-def format_file(src, dst=None):
+def format_file(src: str | pathlib.Path, dst: str | pathlib.Path | None = None) -> None:
     if dst is None:
         dst = src
 
@@ -37,5 +40,5 @@ def format_file(src, dst=None):
 @click.command()
 @click.argument("src", type=click.Path(exists=True))
 @click.argument("dst", type=click.File("w"), required=False)
-def main(src, dst=None):
+def main(src: str, dst: str = None):
     format_file(src, dst)
--- tests/test_nb_code_formatter.py
+++ tests/test_nb_code_formatter.py
@@ -5,43 +5,39 @@ from click.testing import CliRunner
 import nbformat
 import nb_code_formatter
 
-# from nb_code_formatter import __version__
-
-
-def test_version():
-    print("test_version")
-    assert nb_code_formatter.__version__ == "0.1.0"
-
 
 def test_cli():
-    print("test_cli")
     runner = CliRunner()
-    
+
     with tempfile.NamedTemporaryFile() as tmpf:
         result = runner.invoke(
             nb_code_formatter.main, ["tests/sample.ipynb", tmpf.name]
         )
         assert result.exit_code == 0
-        nb = nbformat.read(
-            tmpf.name, as_version=nb_code_formatter.NBFORMAT_VERSION
-        )
+        nb = nbformat.read(tmpf.name, as_version=nb_code_formatter.NBFORMAT_VERSION)
         assert nb["cells"][1]["source"] == 'spam = "ham" * 3\n'
-        assert nb["cells"][3]["source"] == 'def longlonglonglonglonglonglonglonglong_functionname(\n    longlonglonglong_arg1: str = "abc",\n    longlonglonglong_arg2: int = 1,\n    longlonglonglong_arg3: float = 1.1,\n):\n    return arg1 * int(arg2 + arg3)\n'
+        assert (
+            nb["cells"][2]["source"] == "import abc\nimport datetime\n\nimport isort\n"
+        )
+        assert (
+            nb["cells"][3]["source"]
+            == 'def longlonglonglonglonglonglonglonglong_functionname(\n    longlonglonglong_arg1: str = "abc",\n    longlonglonglong_arg2: int = 1,\n    longlonglonglong_arg3: float = 1.1,\n):\n    return arg1 * int(arg2 + arg3)\n'
+        )
 
 
 def test_cli_overwrite():
-    print("test_cli")
     runner = CliRunner()
-    
+
     with tempfile.NamedTemporaryFile() as tmpf:
         shutil.copyfile("tests/sample.ipynb", tmpf.name)
-        result = runner.invoke(
-            nb_code_formatter.main, [tmpf.name]
-        )
+        result = runner.invoke(nb_code_formatter.main, [tmpf.name])
         assert result.exit_code == 0
-        nb = nbformat.read(
-            tmpf.name, as_version=nb_code_formatter.NBFORMAT_VERSION
-        )
+        nb = nbformat.read(tmpf.name, as_version=nb_code_formatter.NBFORMAT_VERSION)
         assert nb["cells"][1]["source"] == 'spam = "ham" * 3\n'
-        assert nb["cells"][2]["source"] == 'import abc\nimport datetime\n\nimport isort\n'
-        assert nb["cells"][3]["source"] == 'def longlonglonglonglonglonglonglonglong_functionname(\n    longlonglonglong_arg1: str = "abc",\n    longlonglonglong_arg2: int = 1,\n    longlonglonglong_arg3: float = 1.1,\n):\n    return arg1 * int(arg2 + arg3)\n'
+        assert (
+            nb["cells"][2]["source"] == "import abc\nimport datetime\n\nimport isort\n"
+        )
+        assert (
+            nb["cells"][3]["source"]
+            == 'def longlonglonglonglonglonglonglonglong_functionname(\n    longlonglonglong_arg1: str = "abc",\n    longlonglonglong_arg2: int = 1,\n    longlonglonglong_arg3: float = 1.1,\n):\n    return arg1 * int(arg2 + arg3)\n'
+        )
