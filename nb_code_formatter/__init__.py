91ffaa8a213da63d5cd1cde6c41cc7e9b7b1c300
driller <eleshis@gmail.com>
2022-12-17 14:55:35 +0900

    add support for magic commands

--- nb_code_formatter/__init__.py
+++ nb_code_formatter/__init__.py
@@ -13,13 +13,57 @@ NBFORMAT_VERSION = 4
 
 
 def black_formatter(cell_text: str) -> str:
-    drop_magic_text: str = re.sub("^%", "#%#", cell_text, flags=re.M)
-    reformated_text: str = black.format_str(drop_magic_text, mode=black.FileMode())
-    return re.sub("^#%#", "%", reformated_text, flags=re.M)
+    return black.format_str(cell_text, mode=black.FileMode())
+
+
+def _format_code(cell_text: str) -> str:
+    return isort.code(black_formatter(cell_text)).rstrip()
+
+
+def _format_linemagic(line: str):
+    pat: re.Pattern = re.compile(r"^(%\w+)\s+(.*)")
+    result: re.Match | None = pat.match(line)
+    magic_command: str = result.group(1)
+    python_code: str = result.group(2)
+    return " ".join((magic_command, black_formatter(python_code)))
+
+
+def _format_magic_code(cell_text: str, code_type: str) -> str:
+    lines: list = cell_text.split("\n")
+    magic: str = lines[0]
+    if code_type == "line_magic":
+        magic: str = _format_linemagic(lines[0])
+    code: str = "\n".join(lines[1:])
+    formated_code: str = _format_code(code)
+    return "\n".join((magic, formated_code)).rstrip()
+
+
+def _format_cellmagic_code(cell_text: str) -> str:
+    return _format_magic_code(cell_text, "cell_magic")
+
+
+def _format_linemagic_code(cell_text: str) -> str:
+    return _format_magic_code(cell_text, "line_matic")
+
+
+def get_code_type(source: str) -> str:
+    if not source:
+        return "pure_python"
+    elif source[:2] == "%%":
+        return "cell_magic"
+    elif source[0] == "%":
+        return "line_magic"
+    else:
+        return "pure_python"
 
 
 def format_code(cell_text: str) -> str:
-    return isort.code(black_formatter(cell_text))
+    code_type: str = get_code_type(cell_text)
+    return {
+        "pure_python": _format_code,
+        "cell_magic": _format_cellmagic_code,
+        "line_magic": _format_linemagic_code,
+    }[code_type](cell_text)
 
 
 def format_nb(nb: NotebookNode) -> NotebookNode:
@@ -33,7 +77,9 @@ def format_file(src: str | pathlib.Path, dst: str | pathlib.Path | None = None) 
     if dst is None:
         dst = src
 
-    nb = nbformat.read(src, as_version=NBFORMAT_VERSION)
+    nb: nbformat.notebooknode.NotebookNode = nbformat.read(
+        src, as_version=NBFORMAT_VERSION
+    )
     nbformat.write(format_nb(nb), dst)
 
 
--- tests/test_nb_code_formatter.py
+++ tests/test_nb_code_formatter.py
@@ -15,14 +15,18 @@ def test_cli():
         )
         assert result.exit_code == 0
         nb = nbformat.read(tmpf.name, as_version=nb_code_formatter.NBFORMAT_VERSION)
-        assert nb["cells"][1]["source"] == 'spam = "ham" * 3\n'
-        assert (
-            nb["cells"][2]["source"] == "import abc\nimport datetime\n\nimport isort\n"
-        )
+        assert nb["cells"][1]["source"] == 'spam = "ham" * 3'
+        assert nb["cells"][2]["source"] == "import abc\nimport datetime\n\nimport isort"
         assert (
             nb["cells"][3]["source"]
-            == 'def longlonglonglonglonglonglonglonglong_functionname(\n    longlonglonglong_arg1: str = "abc",\n    longlonglonglong_arg2: int = 1,\n    longlonglonglong_arg3: float = 1.1,\n):\n    return arg1 * int(arg2 + arg3)\n'
+            == "import abc\nimport datetime\n\nimport isort\n\n1 + 1"
         )
+        assert (
+            nb["cells"][4]["source"]
+            == 'def longlonglonglonglonglonglonglonglong_functionname(\n    longlonglonglong_arg1: str = "abc",\n    longlonglonglong_arg2: int = 1,\n    longlonglonglong_arg3: float = 1.1,\n):\n    return arg1 * int(arg2 + arg3)'
+        )
+        assert nb["cells"][5]["source"] == "%time 10**10"
+        assert nb["cells"][6]["source"] == "%%time\na = 1"
 
 
 def test_cli_overwrite():
@@ -33,11 +37,15 @@ def test_cli_overwrite():
         result = runner.invoke(nb_code_formatter.main, [tmpf.name])
         assert result.exit_code == 0
         nb = nbformat.read(tmpf.name, as_version=nb_code_formatter.NBFORMAT_VERSION)
-        assert nb["cells"][1]["source"] == 'spam = "ham" * 3\n'
-        assert (
-            nb["cells"][2]["source"] == "import abc\nimport datetime\n\nimport isort\n"
-        )
+        assert nb["cells"][1]["source"] == 'spam = "ham" * 3'
+        assert nb["cells"][2]["source"] == "import abc\nimport datetime\n\nimport isort"
         assert (
             nb["cells"][3]["source"]
-            == 'def longlonglonglonglonglonglonglonglong_functionname(\n    longlonglonglong_arg1: str = "abc",\n    longlonglonglong_arg2: int = 1,\n    longlonglonglong_arg3: float = 1.1,\n):\n    return arg1 * int(arg2 + arg3)\n'
+            == "import abc\nimport datetime\n\nimport isort\n\n1 + 1"
         )
+        assert (
+            nb["cells"][4]["source"]
+            == 'def longlonglonglonglonglonglonglonglong_functionname(\n    longlonglonglong_arg1: str = "abc",\n    longlonglonglong_arg2: int = 1,\n    longlonglonglong_arg3: float = 1.1,\n):\n    return arg1 * int(arg2 + arg3)'
+        )
+        assert nb["cells"][5]["source"] == "%time 10**10"
+        assert nb["cells"][6]["source"] == "%%time\na = 1"
